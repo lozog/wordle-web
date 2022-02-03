e9dbbcd08d97fd5318685e7ac26b34b50f9df120
Liam Ozog <liam@ada.support>
2022-02-02 21:55:32 -0500

    make link to homepage open in new tab

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,7 @@ export function Header({ openModal, toggleHardMode, isHardMode }: Props) {
     <S.Container>
       <S.Title>Wordle Clone</S.Title>
       <S.Links>
-        <S.Link href="https://liamozog.com" rel="noopener noreferrer">home</S.Link>
+        <S.Link href="https://liamozog.com" target="_blank" rel="noopener noreferrer">home</S.Link>
         <S.Link href="https://github.com/lozog/wordle-web" target="_blank" rel="noopener noreferrer">source</S.Link>
         <S.LinkButton onClick={openModal}>stats</S.LinkButton>
         <S.LinkButton onClick={toggleHardMode}>{isHardMode ? "disable" : "enable"} hard mode</S.LinkButton>
