0c4524e4644669894b32f105ddc1005a8bd3e09d
Liam Ozog <liam.ozog@gmail.com>
2022-02-18 20:48:26 -0500

    prevent ENTER press from toggling hard mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,7 +16,12 @@ export function Header({ openModal, toggleHardMode, isHardMode }: Props) {
         <S.Link href="https://github.com/lozog/wordle-web" target="_blank" rel="noopener noreferrer">source</S.Link>
         <S.Link href="https://www.powerlanguage.co.uk/wordle/" target="_blank" rel="noopener noreferrer">og wordle</S.Link>
         <S.LinkButton onClick={openModal}>stats</S.LinkButton>
-        <S.LinkButton onClick={toggleHardMode}>{isHardMode ? "disable" : "enable"} hard mode</S.LinkButton>
+        <S.LinkButton
+          onKeyDown={(e) => { e.preventDefault(); }} // prevent ENTER from toggling hard mode
+          onClick={toggleHardMode}
+        >
+          {isHardMode ? "disable" : "enable"} hard mode
+        </S.LinkButton>
       </S.Links>
     </S.Container>
   );
